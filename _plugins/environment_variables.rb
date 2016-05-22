ba5d1e6c9856ab552ecc296576bf94a694adad25
John Tajima <manjiro@gmail.com>
2016-05-22 16:45:20 -0400

    fix config

--- _plugins/environment_variables.rb
+++ _plugins/environment_variables.rb
@@ -3,7 +3,8 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
 
     def generate(site)
-      site.config['env'] = ENV['JEKYLL_ENV'] || 'production'
+      site.config['env'] = ENV['JEKYLL_ENV']
+      site.config['env'] = 'production' if site.config['env'].length == 0
       # Add other environment variables to `site.config` here...
     end
 
